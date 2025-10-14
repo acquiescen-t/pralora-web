b2e4f609542fde3911c1082829fb3ee57a4495dc
Trevor <trevorchew.kh@gmail.com>
2025-10-14 13:43:03 +0800

    Working List Group

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item + " has been selected.");
+  };
   return (
     <div>
-      <Message />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
