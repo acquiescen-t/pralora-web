8ea9da72c15e913240f13347f394d5941d39db0c
Trevor <trevorchew.kh@gmail.com>
2025-10-16 17:04:58 +0800

    Replaced search bar placeholder with a search bar. Logic to search to be implemented later.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,27 @@ import Sidebar from "./Sidebar";
 
 import logoGreen from "../assets/pralora.png";
 import logoWhite from "../assets/pralora-white.png";
+import { MagnifyingGlassIcon } from "@phosphor-icons/react";
+import { useState } from "react";
 
-const Navbar = () => {
+interface Props {
+  initialQuery?: string;
+}
+
+const Navbar = ({ initialQuery = "" }: Props) => {
+  const [query, setQuery] = useState(initialQuery);
+
+  const handleSearch = () => {
+    // To be implemented
+    console.log("Searching for " + query);
+  };
+
   return (
     <nav className="navbar fixed-top">
       <div className="d-flex align-items-center w-100">
         <div className="d-flex align-items-center ps-3" style={{ flex: 1 }}>
           <Sidebar />
-          <Link className="navbar-brand navbar-logo-container ps-3" to="/">
+          <Link className="navbar-brand navbar-logo-container ms-1" to="/home">
             <img src={logoGreen} className="logo logo-green" />
             <img src={logoWhite} className="logo logo-white" />
           </Link>
@@ -20,7 +33,23 @@ const Navbar = () => {
           className="d-flex justify-content-center text-white"
           style={{ flex: 4 }}
         >
-          <p> Search bar goes here </p>
+          <div className="input-group search-bar">
+            <button
+              className="btn btn-outline-secondary"
+              onClick={() => handleSearch()}
+            >
+              <MagnifyingGlassIcon />
+            </button>
+            <input
+              id="Search"
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
         </div>
 
         <div
