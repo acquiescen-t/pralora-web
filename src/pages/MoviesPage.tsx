fa9357c8b3f8f251a88cc158ae7fcd38b1e3c640
Trevor <trevorchew.kh@gmail.com>
2025-10-21 20:21:59 +0800

    Movies page displays a banner like component on top that dynamically changes based on the selected movie. The title, genres, and backdrop images update whenever the user selects a movie.

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import { Endpoints } from "../api/Endpoints";
+import api from "../api/InternalApi";
+import type { Movie } from "../models/Movie";
+import MediaCard from "../components/MediaCard";
+import Config from "../api/Config";
+import type { TvSeries } from "../models/TvSeries";
+import PreviewMedia from "../components/PreviewMedia";
+
 export default function MoviesPage() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  useEffect(() => {
+    api
+      .get(Endpoints.getAllMovies())
+      .then((response) => setMovies(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const [selectedMovie, setSelectedMovie] = useState<Movie>();
+  useEffect(() => {
+    setSelectedMovie(movies[0]);
+  }, [movies]);
+
+  const handleSelectMedia = (media: Movie | TvSeries) => {
+    "title" in media && setSelectedMovie(media);
+  };
+
   return (
-    <div>
-      <h1>Movies Page</h1>
+    <div className="pt-4 ps-3">
+      <PreviewMedia media={selectedMovie!}></PreviewMedia>
+      <div className="container-fluid px-0">
+        <div className="row g-2">
+          {movies.map((movie) => (
+            <div className="col-2">
+              <MediaCard
+                media={movie}
+                onSelectMedia={handleSelectMedia}
+              ></MediaCard>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
