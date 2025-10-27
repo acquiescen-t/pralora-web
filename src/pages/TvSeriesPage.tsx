74c37f804534d25bfbacf1ca46ab59d4262c3cc8
Trevor <trevorchew.kh@gmail.com>
2025-10-27 16:51:04 +0800

    Implemented TvSeriesPage

--- src/pages/TvSeriesPage.tsx
+++ src/pages/TvSeriesPage.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import type { TvSeries } from "../models/TvSeries";
+import api from "../api/InternalApi";
+import { Endpoints } from "../api/Endpoints";
+import type { Movie } from "../models/Movie";
+import MediaCard from "../components/MediaCard";
+import PreviewMedia from "../components/PreviewMedia";
+
 export default function TvSeriesPage() {
+  const [tvSeries, setTvSeries] = useState<TvSeries[]>([]);
+  useEffect(() => {
+    api
+      .get(Endpoints.getAllTvSeries())
+      .then((response) => setTvSeries(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+  useEffect(() => {
+    setSelectedMedia(tvSeries[0]);
+  }, [tvSeries]);
+
+  const [selectedMedia, setSelectedMedia] = useState<Movie | TvSeries>();
+
+  const handleSelectMedia = (media: Movie | TvSeries) => {
+    setSelectedMedia(media);
+  };
+
   return (
     <div>
-      <h1>Tv Series Page</h1>
+      <div className="pt-3 ps-3">
+        <div className="page-header">All Tv Series</div>
+        <div className="row">
+          <div className="col-6 pt-4">
+            <div className="container overflow-auto media-scroll">
+              <div className="row g-3">
+                {tvSeries.map((tv) => (
+                  <div key={tv.id} className="col-12 col-md-3">
+                    <MediaCard
+                      media={tv}
+                      onSelectMedia={handleSelectMedia}
+                    ></MediaCard>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            {selectedMedia && (
+              <PreviewMedia media={selectedMedia}></PreviewMedia>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
