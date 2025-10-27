54f4e5f1c875991126fb56cc82b61e8f1af56277
Trevor <trevorchew.kh@gmail.com>
2025-10-27 14:14:15 +0800

    Conditional rendering for GenrePage. Added header at the top to state how many movies of which genre are being shown

--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import MediaCard from "../components/MediaCard";
 import PreviewMedia from "../components/PreviewMedia";
 import type { TvSeries } from "../models/TvSeries";
+import { GENRES } from "../constants/Genres";
 
 const GenrePage = () => {
   const { genreId } = useParams() as { genreId: string };
@@ -29,27 +30,37 @@ const GenrePage = () => {
   };
 
   return (
-    <div className="pt-3 ps-3">
-      <div className="row">
-        <div className="col-6 pt-4">
-          <div className="container overflow-auto movies-scroll">
-            <div className="row g-3">
-              {movies &&
-                movies.map((movie) => (
-                  <div key={movie.id} className="col-12 col-md-3">
-                    <MediaCard
-                      media={movie}
-                      onSelectMedia={handleSelectMedia}
-                    ></MediaCard>
-                  </div>
-                ))}
+    <div className="py-3 ps-3">
+      {movies && (
+        <>
+          {genreId && (
+            <div className="genre-header">
+              Filtered {movies.length} {GENRES[parseInt(genreId)]} Movies
+            </div>
+          )}
+
+          <div className="row">
+            <div className="col-6 pt-4">
+              <div className="container overflow-auto movies-scroll">
+                <div className="row g-3">
+                  {movies &&
+                    movies.map((movie) => (
+                      <div key={movie.id} className="col-12 col-md-3">
+                        <MediaCard
+                          media={movie}
+                          onSelectMedia={handleSelectMedia}
+                        ></MediaCard>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <PreviewMedia media={selectedMovie!}></PreviewMedia>
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <PreviewMedia media={selectedMovie!}></PreviewMedia>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
