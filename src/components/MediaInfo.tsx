3b82bebe871aebc943e2e8997cf42da4cdeebaad
Trevor <trevorchew.kh@gmail.com>
2025-10-29 00:47:50 +0800

    Laid out content using d-flex instead of rows and columns. Aligned releaseDate and votes to the far right of the container

--- src/components/MediaInfo.tsx
+++ src/components/MediaInfo.tsx
@@ -25,28 +25,26 @@ export default function MediaInfo({ media }: Props) {
   return (
     <div className="card-body p-1 text-white">
       <div className="media-info-title bebas-light py-2">{mediaTitle}</div>
-        <div className="col">
+      <div className="d-flex pt-1">
         <div className="media-info-genres">
-          <div className="preview-genres">
-            {media.genre.map((id) => (
-              <Link key={id} to={"/genres/" + id}>
-                <span className="badge badge-green me-2">{GENRES[id]}</span>
-              </Link>
-            ))}
-          </div>
+          {media.genre.map((id) => (
+            <Link key={id} to={"/genres/" + id}>
+              <span className="badge badge-green me-2 bebas-light">
+                {GENRES[id]}
+              </span>
+            </Link>
+          ))}
         </div>
-        <div className="col d-flex justify-content-end align-items-end">
-          <div className="preview-stats">
-            <CalendarBlankIcon size={24} className="me-2" />
-            {new Date(mediaReleaseDate).getFullYear()}
-            <PopcornIcon size={24} className="ms-4 me-2" />
-            {Math.round(media.vote_average * 10) / 10}
-            <UserIcon size={24} className="ms-4 me-2" />
-            {media.vote_count} votes
-          </div>
-      <div className="media-info-overview py-2">{media.overview}</div>
+        <div className="preview-stats ms-auto">
+          <CalendarBlankIcon size={24} className="me-2" />
+          {new Date(mediaReleaseDate).getFullYear()}
+          <PopcornIcon size={24} className="ms-4 me-2" />
+          {Math.round(media.vote_average * 10) / 10}
+          <UserIcon size={24} className="ms-4 me-2" />
+          {media.vote_count} votes
+        </div>
       </div>
-      <div className="preview-overview py-2">{media.overview}</div>
+      <div className="media-info-overview py-2">{media.overview}</div>
     </div>
   );
 }
