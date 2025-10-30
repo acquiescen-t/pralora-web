29dd34dcc183fc5588d440c4ebc04284842a06c1
Trevor <trevorchew.kh@gmail.com>
2025-10-30 14:26:51 +0800

    Implemented page to display all Actors

--- src/pages/ActorsPage.tsx
+++ src/pages/ActorsPage.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Person } from "../models/Person";
+import api from "../api/InternalApi";
+import { Endpoints } from "../api/Endpoints";
+import PersonCard from "../components/PersonCard";
+
 export default function ActorsPage() {
+  const [actors, setActors] = useState<Person[]>([]);
+  useEffect(() => {
+    api
+      .get(Endpoints.getAllPersons())
+      .then((response) => setActors(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div>
-      <h1>Actors Page</h1>
+    <div className="cast-info">
+      <div className="page-header">All Actors</div>
+      <div className="row p-3 g-4">
+        {actors.map((actor) => (
+          <PersonCard key={actor.tmdbId} personDetails={actor} />
+        ))}
+      </div>
     </div>
   );
 }
